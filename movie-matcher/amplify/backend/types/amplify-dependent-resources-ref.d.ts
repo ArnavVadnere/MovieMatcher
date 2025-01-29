e6efdb39bdd977537d72fa759c04be508e52d1aa
ArnavVadnere <arnavvadnere@gmail.com>
2025-01-28 23:38:25 -0500

    Add sign up functionality

---
+++ movie-matcher/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "moviematchera5c29dcc": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
